04fb93536d04994e2b33e0661ef813d6a36ed75c
brian <B06504102@ntu.edu.tw>
2020-10-01 16:41:27 +0800

    add Dijkstra

--- Graph.cpp
+++ Graph.cpp
@@ -12,6 +12,90 @@
 #include <utility>
 using namespace std;
 # define infty 2147483647
+
+struct Priority_queue{
+    int size;
+    int *vertexIndex; // the index in priority queue is not same as the vertex no.
+    vector<pair<int,int>> pq; // < key , vertex no. >
+
+    int Leftchild(int i){return 2*i;}
+
+    int Parent(int i){return i/2;}
+
+    void Heapify(int i){
+        int j = Leftchild(i);
+        pair<int,int> key = pq[i];
+        while(j<=size){// leftchild exist
+            if(j+1 <= size) {// rightchild exist
+                if(pq[j].first>pq[j+1].first)
+                    j++;
+            }
+            /* j = index of min{lchild,rchild} */
+            if(key.first>pq[j].first){ // arr[j] move to index i
+                vertexIndex[pq[j].second] = i;
+                pq[i]= pq[j];
+            }else // arr[i] is already minimum
+                break;
+            i = j;
+            j = Leftchild(i);
+        }
+        vertexIndex[key.second] = i;
+        pq[i]= key;
+    }
+    
+    Priority_queue(int n, int *key){ // make heap
+        this->size = n;
+        vertexIndex = new int[n+1];
+        pq.push_back({0,0});
+        for(int i=1;i<=n;i++){
+            vertexIndex[i] = i;
+            pq.push_back({key[i],i});
+        }
+        /* make heap */
+        for(int i=Parent(n);i>0;i--){
+          Heapify(i);
+        }
+    }
+
+    void Decrease_key(int i, int k){
+        int x = vertexIndex[i]; // converting vertex no. to priority queue index
+        pq[x].first = k;
+        pair<int,int> key = pq[x];
+
+        if(x == 1) // decrease-key of root, dont need to modify
+            return;
+        
+        while(x>0){
+            int p = Parent(x);
+            if(pq[p].first > k){
+                vertexIndex[pq[p].second] = x;
+                pq[x] = pq[p];
+            }else
+                break;
+            x = p;
+        }
+        vertexIndex[i] = x;
+        pq[x] = key;
+    }
+
+    bool Empty(){
+        return size == 0;
+    }
+    int Extract_min(){
+        int min = pq[1].second;
+        vertexIndex[pq[size].second] = 1;
+        pq[1] = pq[size];
+        this->size--;
+        Heapify(1);
+        return min;
+    }
+
+    void printQueue(){
+        for(int i=1;i<=5;i++)
+            printf("%d\n",pq[i].first);
+    }
+};
+
 struct vertex{
     int number, distance, discovery, finish, pi;
     char color;
@@ -25,22 +109,29 @@ class Graph{
     list<int> T_sort;
     vector<vertex> Vertice;
     vector<list<int>> Adjlist;
+    vector<list<pair<int,int>>> w_Adjlist; // <vertex, weight>
     
     Graph(int n){
         this->V = n;
         this->t = 0;
         Vertice.resize(n+1);
         Adjlist.resize(n+1); // the vertex no. could be 1~n, not including 0
+        w_Adjlist.resize(n+1); // the vertex no. could be 1~n, not including 0
     }
     void addEdge_undirected(int u, int v);
     void addEdge_directed(int u, int v);
+    void addEdge_undirected_weighted(int u, int v, int w);
+    void addEdge_directed_weighted(int u, int v, int w);
     void BFS(int start);
     void DFS_visit(int u);
     void DFS(int start);
+    bool Relax(int u, int v, int w);
+    void Dijkstra(int start);
     void SCC();
     void Topological_Sort(int start);
     int Diameter();
     void printGraph();
+    void printGraph_weighted();
 };
 
 void Graph::addEdge_undirected(int u, int v){
@@ -51,6 +142,16 @@ void Graph::addEdge_undirected(int u, int v){
 void Graph::addEdge_directed(int u, int v){
     Adjlist[u].push_back(v);
 }
+
+void Graph::addEdge_undirected_weighted(int u, int v, int w){
+    w_Adjlist[u].push_back({v,w});
+    w_Adjlist[v].push_back({u,w});
+}
+
+void Graph::addEdge_directed_weighted(int u, int v, int w){
+    w_Adjlist[u].push_back({v,w});
+}
+
 
 void Graph::BFS(int start){
     for(int i=1;i<=V;i++){
@@ -104,6 +205,40 @@ void Graph::DFS(int start){
             DFS_visit(i);
     }
     this->t = 0;
+}
+
+bool Graph::Relax(int u, int v, int w){
+    if(Vertice[v].distance > Vertice[u].distance + w){
+        Vertice[v].distance = Vertice[u].distance + w;
+        return true;
+    }
+    return false;
+}
+
+void Graph::Dijkstra(int start){ // still testing
+    int *key = new int[V];
+    for(int i=1;i<=V;i++){
+        key[i] = infty;
+        Vertice[i].number=i;
+        Vertice[i].distance = infty;
+        Vertice[i].pi = -1;
+    }
+    Priority_queue Q(V,key);
+    Vertice[start].distance = 0;
+    Q.Decrease_key(start,0);
+    while(!Q.Empty()){
+        int u = Q.Extract_min();
+        for(list<pair<int,int>>::iterator it=w_Adjlist[u].begin(), end=w_Adjlist[u].end();it!=end;it++){
+            int v = it->first, w = it->second;
+            if(Relax(u,v,w)){
+                Q.Decrease_key(it->first,Vertice[v].distance);
+                Vertice[it->first].pi = u;
+            }
+        }
+    }
+    for(int i=1;i<=V;i++){
+        printf("%d : %d\n",Vertice[i].number,Vertice[i].distance);
+    }
 }
 
 void Graph::SCC(){
@@ -180,13 +315,25 @@ void Graph::printGraph(){
     }
 }
 
+void Graph::printGraph_weighted(){
+    for(int i=1;i<=V;i++){
+        printf("Adj[%d] : ",i);
+        for(list<pair<int,int>>::iterator it = w_Adjlist[i].begin();it!=w_Adjlist[i].end();it++){
+            printf("%d ",it->first);
+        }
+        printf("\n");
+    }
+}
+
 int main(){
     int V;
     cin >> V;
     Graph G(V);
-    int u,v;
-    while(cin >> u >> v){
-        G.addEdge_directed(u,v); // or undirected
+    int u,v,w;
+    while(cin >> u >> v >> w){
+        G.addEdge_directed_weighted(u,v,w); // or undirected
     }
+    G.Dijkstra(1);
+
     return 0;
 }
